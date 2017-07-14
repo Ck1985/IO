3dc0ba09fc8125ab3d41c480582ac44fda420636
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-07-14 16:54:16 +0700

    New Commit

---
+++ src/io/OSExecute.java
@@ -0,0 +1,12 @@
+package io;
+
+import java.io.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class OSExecute {
+    public static void command(String command) {
+
+    }
+}
---
+++ src/io/OSExecuteException.java
@@ -0,0 +1,10 @@
+package io;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class OSExecuteException extends RuntimeException {
+    public OSExecuteException(String why) {
+        super(why);
+    }
+}
