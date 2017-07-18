38fce364c6dbf8a932c9a6f8fa339cbed4d95b55
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-07-18 17:10:27 +0700

    New Commit

--- src/io/LargeMappedFiles.java
+++ src/io/LargeMappedFiles.java
@@ -9,4 +9,16 @@ import java.util.*;
  * Created by anonymous.vn1985@gmail.com
  */
 public class LargeMappedFiles {
+    private static final int length = 0x8FFFFFF;
+    public static void main(String[] args) throws IOException {
+        MappedByteBuffer out = new RandomAccessFile("C:\\Users\\anony\\Documents\\Directory_Data\\Data\\Output_Text.txt","rw").getChannel().map(FileChannel.MapMode.READ_WRITE,0,length);
+        for (int i = 0; i < length; i++) {
+            out.put((byte)'x');
+        }
+        System.out.println("Finishing writing");
+        for (int i = length / 2; i < length / 2 + 6; i++) {
+            System.out.print((char)out.get(i) + " ");
+        }
+        System.out.println();
+    }
 }
